e5ff1becdd65c8398ee9c498a739b93f276a40c9
oka <pn11@users.noreply.github.com>
2019-06-02 18:48:52 +0900

    change install script

---
+++ install.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+filelist = [
+  ".bash_profile",
+  ".bashrc",
+  ".emacs",
+  ".gnuplot",
+  ".lesskey",
+  ".screenrc",
+  ".tmux.conf",
+  ".gitconfig"
+]
+
+
+def func(filename)
+  home = ENV['HOME'].chomp
+  target = "#{home}/#{filename}"
+  pwd = `pwd`.chomp
+  
+  if File.exist?(target)
+    puts "#{target} exists. Instllation canceled."
+   
+  else
+    `ln -s #{pwd}/#{filename} #{target}`
+    puts "done" 
+  end
+end
+
+
+# main routine
+for i in 0..filelist.size()-1
+  puts "Installing #{filelist[i]}"
+  func filelist[i]
+end
+
+
+
+
