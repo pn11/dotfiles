bed5c656be03e8a6e6cd3e0235d62ffe513238fd
oka <pn11@users.noreply.github.com>
2020-05-21 23:16:14 +0900

    add 32 bit automation

---
+++ LinOps/README.md
@@ -0,0 +1,4 @@
+# Linux の環境構築用スクリプト
+
+- `apt-32bit.sh`
+  - 32 bit distro で ruby 2.6 が install できず、Homebrew が使えなかったため、apt-get と pip で入れたものを記録。
---
+++ LinOps/apt-32bit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install \
+     git \
+     python3-pip \
+     python3-venv \
+     ruby \
+     emacs \
+     skktools \
+     tmux
+
+pip3 install \
+     wakatime \
+     adafruit-ampy
