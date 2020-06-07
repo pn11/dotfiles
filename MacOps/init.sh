1ed4fb539f10e493e980dbb34b1919fcc1ffde46
oka <oka@stu.kobe-u.ac.jp>
2020-06-07 18:38:58 +0900

    update macops

--- MacOps/init.sh
+++ MacOps/init.sh
@@ -2,6 +2,39 @@
 
 ## Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+if [ ! -e ~/git ]; then
+    mkdir ~/git
+fi
+
+function clone_repo() {
+    dirname=~/git/$(echo ${1} | sed 's/.*\///g')
+    if [ -e $dirname ]; then
+        return
+    fi
+    pushd ~/git
+    git clone git@github.com:${1}.git ${dirname}
+    popd
+}
+
+clone_repo pn11/dotfiles
+pushd ~/git/dotfiles
+ruby install.rb
+popd
+
+clone_repo pn11/dotfiles-private
+pushd ~/git/dotfiles-private
+ruby install.rb
+popd
+
+clone_repo pn11/benkyokai
+clone_repo pn11/cheat
+clone_repo pn11/location-grass
+clone_repo pn11/OkaNotes
+clone_repo pn11/OkaScripts
+clone_repo pn11/pn11.github.io
+
+pushd dotfiles/MacOps
 brew update
 cat Homebrew/mojave.brew.list | xargs brew install
 cat Homebrew/mojave.cask.list | xargs brew cask install
